23ce27d960a7d4f7a34816dfaafdef8e4f671f22
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2016-05-08 11:22:00 +0530

    Add project files.

---
+++ AutoCompleteApp/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AutoCompleteApp
+{
+    [Activity(Label = "Technologies", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            string[] technologies = {
+                "Android",
+                "ASP.NET MVC",
+                "ASP.NET Core",
+                "Azure",
+                "Angular JS",
+                "Big Data",
+                "Database",
+                "HTML5",
+                "Internet of Things",
+                "Machine Learning",
+                "SQL Server",
+                "Xamarin"};
+
+            var autoCompleteTextView = FindViewById<AutoCompleteTextView>(Resource.Id.autoCompleteTextView);
+
+            var adapter = new ArrayAdapter<string>(this, Resource.Layout.ListItem, technologies);
+
+            autoCompleteTextView.Adapter = adapter;
+        }
+    }
+}
+
